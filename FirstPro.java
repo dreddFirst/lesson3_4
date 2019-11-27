b150acb8128eb2fce31afc86bb67e56743fbd57d
dredd <treba2005@mail.ru>
2019-11-27 11:15:00 +0200

    create and clone

---
+++ FirstPro.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author treba
+ */
+public class FirstPro 
+{
+    public static void main(String[] args)
+    {
+    
+    System.out.println("hi Piter!");
+    System.out.println(args[0]);
+    System.out.println(args[1]);
+    }
+}
